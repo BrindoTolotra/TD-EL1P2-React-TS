8e3d29e83d5794b662eab1e7880c81bf76fdac80
Brindo RATIHARIMANANA <hei.brindo.2@gmail.com>
2022-07-06 22:26:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useRef, useState } from "react";
+import { Input } from "./Components/Input";
+import { Table } from "./Components/Table";
+
+
+// React.FC = App type React Fonction Composant
+const App: React.FC = () => {
+    
+    
+
+
+    return (
+        <>
+            <Input/>
+            <div  className="row">
+                <Table titre="TODO" className="col-4"/>
+                <Table titre="DOING" className="col-4"/>
+                <Table titre="DONE" className="col-4"/>
+            </div>
+        </>
+    )
+}
+
+export default App;
